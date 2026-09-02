ad563db53a8ad3f877c4b1e3a9876f3347c88338
Simran kaur <sk6741296@gmail.com>
2026-09-02 13:53:40 +0530

    Configure explicit Webpack path alias for @ in next.config.js to fix Vercel Linux build resolution

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
